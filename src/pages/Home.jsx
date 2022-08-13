95530f50d8c3c587570e5e471adc50ea055da4da
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-13 17:35:16 -0400

    Working on delete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import React, { useRef } from 'react'
 import { firestore } from '../firebase'
-import { addDoc, getDocs, collection } from "firebase/firestore"
+import { addDoc, getDocs, collection, deleteDoc, doc } from "firebase/firestore"
 
 export default function Home() {
   const itemRef = useRef()
   const quantityRef = useRef()
   const ref = collection(firestore, "grocery-list");
 
-  const handleSave = async(e) => {
+  const handleAdd = async(e) => {
     e.preventDefault();
     console.log(itemRef.current.value);
 
-    let data = {
+    const data = {
       item: itemRef.current.value,
       quantity: quantityRef.current.value
     }
@@ -29,9 +29,31 @@ export default function Home() {
       console.error(err.message);
     }
   }
+
+  const handleDelete = async(e) => {
+    e.preventDefault();
+    const data = {
+      item: itemRef.current.value,
+      quantity: quantityRef.current.value,
+      id: itemRef.current.value
+    }
+    console.log('deleted', data);
+    const docRef = doc(firestore, 'grocery-list', data.id)
+      .then((snapshot) => {
+        snapshot.docs.filter((doc) => {
+          return doc.id !== data.id;
+        })
+      })
+    try {
+      deleteDoc(docRef);
+    } catch(err) {
+      console.error(err.message);
+    }
+  }
+
   return (
     <div>
-      <form onSubmit={handleSave}>
+      <form onSubmit={handleAdd}>
         <div>
           <label>Enter grocery item</label>
           <input type="text" name="item" ref={itemRef} required />
@@ -40,10 +62,10 @@ export default function Home() {
           <label>Enter quantity</label>
           <input type="number" name="quantity" ref={quantityRef} required/>
         </div>
-        <button type="submit">Save</button>
+        <button type="submit">Add Item</button>
       </form>
 
-      <form>
+      <form onSubmit={handleDelete}>
         <label>Delete</label>
         <input type="text" name="id" required />
         <button>Delete Item</button>
